3be26b666740a090f4728fb0e5944dbf272a2446
MeherSangeeth <sangeethmeher@gamil.com>
2020-08-04 15:16:05 +0530

    First commit

---
+++ Demo.py
@@ -0,0 +1,12 @@
+def sq_num(num):
+    for x in num:
+	print(x*x)
+
+def do_num(num):
+    for x in num:
+	print(x+x)
+
+if __name__ == '__main__':
+    lst=[1,2,3,4,5]
+    sq_num(lst)
+    do_num(lst)
